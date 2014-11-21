657d221d5d342d605324bd7e39dbdadef1273743
Yanick Landry <yanick.landry@gmail.com>
2014-11-21 17:38:25 -0500

    minor fixes : readme, app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+var express = require('express');
+var dummyjson = require('dummy-json');
+
+var template = fs.readFileSync('template.hbs', {encoding: 'utf8'});
+var app = express();
+
+app.get('/deed', function(req, res) {
+	// res.set('Content-Type', 'application/json');
+	// res.send(dummyjson.parse(template));
+
+	res.attachment('deed.json')
+	//following line is not necessary, just experimenting
+	res.setHeader('Content-Type', 'application/octet-stream')
+
+	res.setHeader('Content-Type', 'application/json');
+	res.end(dummyjson.parse(template));
+});
+
+app.listen(3000);
