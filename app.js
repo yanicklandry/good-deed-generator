2647f4cd01649c5abccfe806f157cd0b5af6ad56
Yanick Landry <yanick.landry@gmail.com>
2014-11-21 21:59:59 -0500

    support for multiple models

--- app.js
+++ app.js
@@ -2,17 +2,26 @@ var fs = require('fs');
 var express = require('express');
 var dummyjson = require('dummy-json');
 
-var template = fs.readFileSync('template.hbs', {encoding: 'utf8'});
 var app = express();
 
-app.get('/', function(req, res) {
-	res.send('<a href="./deed">deed.json</a>');
+var models = ['deed'];
+
+models.forEach(function(model) {
+	app.get('/' + model, function(req, res) {
+		var template = fs.readFileSync(model + '.hbs', {encoding: 'utf8'});
+		res.attachment(model + '.json')
+		res.setHeader('Content-Type', 'application/octet-stream')
+		res.end(dummyjson.parse(template));
+	});
 });
 
-app.get('/deed', function(req, res) {
-	res.attachment('deed.json')
-	res.setHeader('Content-Type', 'application/octet-stream')
-	res.end(dummyjson.parse(template));
+app.get('/', function(req, res) {
+	var html = '<ul>'
+	models.forEach(function(model) {
+		html += '<li><a href="./' + model + '">' + model + '.json</a></li>';
+	});
+	html += '</ul>';
+	res.send(html);
 });
 
 app.listen(process.env.PORT || 3000);
