7b5b819c83f27a8eef781bab3542b19b8cb3296a
Yanick Landry <yanick.landry@gmail.com>
2014-11-21 22:53:43 -0500

    added user model, fixed deed model, added jsonlint

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 var fs = require('fs');
 var express = require('express');
 var dummyjson = require('dummy-json');
+var jsonlint = require("jsonlint");
 
 var app = express();
 
-var models = ['deed'];
+var models = ['deed', 'user'];
+
+var domains = ['com', 'net', 'org', 'gov', 'qc.ca'];
+
+var random = function(low, high) {
+	return Math.floor(Math.random() * (high - low) + low);
+};
+
+var helpers = {
+	domain: function(options) {
+		return domains[random(0,domains.length)];
+	}
+};
 
 models.forEach(function(model) {
 	app.get('/' + model, function(req, res) {
 		var template = fs.readFileSync(model + '.hbs', {encoding: 'utf8'});
-		res.attachment(model + '.json')
-		res.setHeader('Content-Type', 'application/octet-stream')
-		res.end(dummyjson.parse(template));
+		res.attachment(model + '.json');
+		res.setHeader('Content-Type', 'application/octet-stream');
+		var json = dummyjson.parse(template, {helpers: helpers});
+		jsonlint.parse(json);
+		res.end(json);
 	});
 });
 
