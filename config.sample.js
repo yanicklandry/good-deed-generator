315b141683162d4146f75451c2fab47af985b10e
Yanick Landry <yanick.landry@gmail.com>
2014-11-22 21:43:30 -0500

    #10 added Parse master key

--- config.sample.js
+++ config.sample.js
@@ -1,5 +1,6 @@
 module.exports = {
-  PARSE_APP_ID: "", //get at https://parse.com/apps/******/edit#keys
+  PARSE_APP_ID: "", //get the 3 Parse keys at https://parse.com/apps/******/edit#keys
   PARSE_REST_API_KEY: "",
+  PARSE_MASTER_KEY: "",
   PARSE_COMMANDS_MAX: 50,
 };
